013d8908b60df5c6a461dce2f9802e9f361cd033
Edoardo <damicoedoardo@virgilio.it>
2018-04-22 18:00:53 +0200

    bug_fixed

--- Neural Network/ModelBuilder.py
+++ Neural Network/ModelBuilder.py
@@ -7,8 +7,8 @@ class ModelBuilder:
     """
     input for the method is a vector []
     in each position there is another vector with one of the following structure:
-        'convolution', conv_depth, kernel_size, activation
-        'pooling', pool_size
+        'convolution', conv_depth, conv_depth kernel_size, activation
+        'pooling', pool_size, pool_size
         'dropout', dropout_probability
         'flatten'
         'dense', hidden_size, activation
@@ -20,21 +20,21 @@ class ModelBuilder:
             ms = model_structure[i]
             if ms[0] == 'conv':
                 if i == 0:
-                    if len(ms) == 4:
-                        model.add(Convolution2D(ms[1], (ms[2], ms[2]), input_shape=input_shape))
-                        model.add(Activation(ms[3]))
+                    if len(ms) == 5:
+                        model.add(Convolution2D(ms[1], (ms[2], ms[3]), input_shape=input_shape))
+                        model.add(Activation(ms[4]))
                     else:
-                        model.add(Convolution2D(ms[1], (ms[2], ms[2]), input_shape=input_shape))
+                        model.add(Convolution2D(ms[1], (ms[2], ms[3]), input_shape=input_shape))
                         model.add(Activation('relu'))
                 else:
-                    if len(ms) == 4:
-                        model.add(Convolution2D(ms[1], (ms[2], ms[2])))
-                        model.add(Activation(ms[3]))
+                    if len(ms) == 5:
+                        model.add(Convolution2D(ms[1], (ms[2], ms[3])))
+                        model.add(Activation(ms[4]))
                     else:
-                        model.add(Convolution2D(ms[1], (ms[2], ms[2])))
+                        model.add(Convolution2D(ms[1], (ms[2], ms[3])))
                         model.add(Activation('relu'))
             elif ms[0] == 'pool':
-                model.add(MaxPooling2D(pool_size=(ms[1], ms[1])))
+                model.add(MaxPooling2D(pool_size=(ms[1], ms[2])))
             elif ms[0] == 'dropout':
                 model.add(Dropout(ms[1]))
             elif ms[0] == 'flatten':
@@ -61,8 +61,8 @@ class ModelBuilder:
         return out
 
 
-# a = [['conv', 16, 3], ['pool', 3], ['flatten'], ['dense', 128], ['dense', 128]]
-a = ModelBuilder.read_model("models/model1.txt")
+a = [['conv', 16, 3, 3], ['pool', 3, 3], ['flatten'], ['dense', 128], ['dense', 128]]
+#a = ModelBuilder.read_model("models/model1.txt")
 size = (80, 80, 2)
 modelObject = ModelBuilder(a, size)
 print(modelObject.model.summary())
