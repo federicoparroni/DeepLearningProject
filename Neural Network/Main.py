1165dceee49a59f45f830c0466aa1205da21ed6d
Edoardo <damicoedoardo@virgilio.it>
2018-04-22 19:27:42 +0200

    Model_Builder bug fixed

--- Neural Network/Main.py
+++ Neural Network/Main.py
@@ -1,14 +1,14 @@
 import numpy as np
 import tensorflow as tf
 import keras.callbacks
-from keras.models import Model
-from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Flatten
 from keras.utils import np_utils
 from keras.callbacks import EarlyStopping
 
 from Utils import current_datetime
 
 import telegram
+from ModelBuilder import read_model
+import ModelBuilder
 
 from LoadData import GetData
 from FaceSequence import FaceSequence
@@ -17,9 +17,10 @@ from ValidationCallback import  ValidationCallback
 
 # ====================CONFIGURING GPU ========================================
 config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
+config.gpu_options.allow_growth = False
 
 # ============================================================================
+
 enable_telegram_bot = True
 # chat_id = 125016709               # this is my private chat id
 # chat_id = "@gdptensorboard"       # this is the name of the public channel
@@ -35,13 +36,6 @@ validation_folders = 12
 
 batch_size = 128            # in each iteration, we consider 128 training examples at once
 num_epochs = 180            # we iterate 200 times over the entire training set
-kernel_size = 3             # we will use 3x3 kernels throughout
-pool_size = 2               # we will use 2x2 pooling throughout
-conv_depth_1 = 16           # we will initially have 32 kernels per conv. layer...
-conv_depth_2 = 16           # ...switching to 64 after the first pooling layer
-drop_prob_conv = 0.1        # dropout after pooling with probability 0.25
-drop_prob_hidden = 0.3      # dropout in the FC layer with probability 0.5
-hidden_size = 128           # the FC layer will have 512 neurons
 
 # (X_train, y_train), (X_test, y_test) = (GetData(TRAINING_DATASET_FOLDER_NAME, limit_on_fonders_to_fetch = True, limit_value = 4), GetData(TEST_DATASET_FOLDER_NAME)) # fetch data
 (X_validation, y_validation, validation_folders_list) = GetData(TRAINING_DATASET_FOLDER_NAME, limit_value=validation_folders)
@@ -64,33 +58,11 @@ Y_train = np_utils.to_categorical(y_train, num_classes)             # One-hot en
 # Y_test = np_utils.to_categorical(y_test, num_classes)             # One-hot encode the labels
 Y_validation = np_utils.to_categorical(y_validation, num_classes)   # One-hot encode the labels
 
-
-inp = Input(shape=(height, width, depth))
-
-# Conv [32] -> Conv [32] -> Pool (with dropout on the pooling layer)
-conv_1 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(inp)
-pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_1)
-drop_1 = Dropout(drop_prob_conv)(pool_1)
-conv_2 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(drop_1)
-pool_2 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_2)
-drop_2 = Dropout(drop_prob_conv)(pool_2)
-conv_3 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(drop_2)
-pool_3 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_3)
-drop_3 = Dropout(drop_prob_conv)(pool_3)
-conv_4 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(drop_3)
-
-# Now flatten to 1D, apply FC -> ReLU (with dropout) -> softmax
-flat = Flatten()(conv_4)
-
-hidden = Dense(hidden_size, activation='relu')(flat)
-drop_4 = Dropout(drop_prob_hidden)(hidden)
-hidden2 = Dense(hidden_size, activation='relu')(drop_4)
-drop_5 = Dropout(drop_prob_hidden)(hidden2)
-hidden3 = Dense(hidden_size, activation='relu')(hidden2)
-
-out = Dense(num_classes, activation='softmax')(hidden3)
-
-model = Model(inputs=inp, outputs=out)      # To define a model, just specify its input and output layers
+#load the model
+a = read_model("models/model1.txt")
+modelObject = ModelBuilder.ModelBuilder(a, (height, width, depth))
+model = modelObject.model
+print(model.summary())
 
 model.compile(loss='categorical_crossentropy',  # using the cross-entropy loss function
               optimizer='adam',                 # using the Adam optimiser
@@ -118,8 +90,6 @@ validation_callback = ValidationCallback(X_validation, Y_validation, 5, chat_id=
 facesequence = FaceSequence(X_train, Y_train, batch_size, TRAINING_DATASET_FOLDER_NAME, epochs_with_same_data=epochs_with_same_data,
                             folders_at_the_same_time = folders_at_the_same_time,
                             to_avoid=validation_folders_list, enable_telegram_bot=enable_telegram_bot, chat_id=chat_id)
-
-print(model.summary())
 
 model.fit_generator(facesequence, epochs=num_epochs,
                     callbacks=[keras.callbacks.LambdaCallback(on_epoch_begin=lambda batch, logs: facesequence.on_epoch_begin()), validation_callback])
--- Neural Network/ModelBuilder.py
+++ Neural Network/ModelBuilder.py
@@ -16,23 +16,19 @@ class ModelBuilder:
 
     def __init__(self, model_structure, input_shape):
         model = Sequential()
-        for i in range(0, len(model_structure)-1):
+        for i in range(0, len(model_structure)):
             ms = model_structure[i]
             if ms[0] == 'conv':
                 if i == 0:
                     if len(ms) == 5:
-                        model.add(Convolution2D(ms[1], (ms[2], ms[3]), input_shape=input_shape))
-                        model.add(Activation(ms[4]))
+                        model.add(Convolution2D(ms[1], (ms[2], ms[3]), activation=ms[4], padding='same', input_shape=input_shape))
                     else:
-                        model.add(Convolution2D(ms[1], (ms[2], ms[3]), input_shape=input_shape))
-                        model.add(Activation('relu'))
+                        model.add(Convolution2D(ms[1], (ms[2], ms[3]), activation='relu', padding='same', input_shape=input_shape))
                 else:
                     if len(ms) == 5:
-                        model.add(Convolution2D(ms[1], (ms[2], ms[3])))
-                        model.add(Activation(ms[4]))
+                        model.add(Convolution2D(ms[1], (ms[2], ms[3]), activation=ms[4], padding='same'))
                     else:
-                        model.add(Convolution2D(ms[1], (ms[2], ms[3])))
-                        model.add(Activation('relu'))
+                        model.add(Convolution2D(ms[1], (ms[2], ms[3]), activation='relu', padding='same'))
             elif ms[0] == 'pool':
                 model.add(MaxPooling2D(pool_size=(ms[1], ms[2])))
             elif ms[0] == 'dropout':
@@ -41,36 +37,37 @@ class ModelBuilder:
                 model.add(Flatten())
             elif ms[0] == 'dense':
                 if len(ms) == 3:
-                    model.add(Dense(ms[1]))
-                    model.add(Activation(ms[2]))
+                    model.add(Dense(ms[1], activation=ms[2]))
                 else:
-                    model.add(Dense(ms[1]))
-                    model.add(Activation('relu'))
+                    model.add(Dense(ms[1], activation='relu'))
         self.model = model
 
-    @staticmethod
-    def read_model(filepath):
-        out = []
 
-        with open(filepath) as file:
-            for line in file.readlines():
-                line = line.strip()
-                if line and not line.startswith("#"):
-                    fields = line.split(",")
-                    # cast
-                    v = [fields[0]]
-                    for i in range(1, len(fields)):
-                        if v[0] == "dropout":
-                            v.append(float(fields[i]))
-                        else:
-                            v.append(int(fields[i]))
+def read_model(filepath):
+    out = []
 
-                    out.append(v)
-        return out
+    with open(filepath) as file:
+        for line in file.readlines():
+            line = line.strip()
+            if line and not line.startswith("#"):
+                fields = line.split(",")
+                # cast
+                v = [fields[0]]
+                for i in range(1, len(fields)):
+                    if v[0] == "dropout":
+                        v.append(float(fields[i]))
+                    elif v[0] == 'dense' and i == 2:
+                        v.append(fields[i])
+                    else:
+                        v.append(int(fields[i]))
 
+                out.append(v)
+    return out
 
-a = [['conv', 16, 3, 3], ['pool', 3, 3], ['flatten'], ['dense', 128], ['dense', 128]]
-#a = ModelBuilder.read_model("models/model1.txt")
+"""
+#a = [['conv', 16, 3, 3], ['pool', 3, 3], ['flatten'], ['dense', 128], ['dense', 128]]
+a = ModelBuilder.read_model("models/model1.txt")
 size = (80, 80, 2)
 modelObject = ModelBuilder(a, size)
 print(modelObject.model.summary())
+"""
