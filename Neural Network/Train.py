0c1b794c9cab4cc377973f1f61e617bcba99dea2
Federico Parroni <keyblade95@live.it>
2018-04-26 15:53:12 +0200

    Debug prints, cleaned code

--- Neural Network/Train.py
+++ Neural Network/Train.py
@@ -9,6 +9,7 @@ from Utils import telegram_send_msg
 
 _instances = {}
 
+
 class SingletonTrain(object):
 
     def __new__(cls, *args, **kw):
@@ -20,16 +21,13 @@ class SingletonTrain(object):
 
     def Train_Sequence(self, model, train_sequence, num_epochs=200, chat_id="undefined", training_callbacks=[], save_model=True):
         if chat_id != "undefined":
-            bot = telegram.Bot(token='591311395:AAEfSH464BdXSDezWGMZwdiLxLg2_aLlGDE')
-            dir(bot)
-            bot.send_message(chat_id=chat_id, text="{} - Training iniziato...".format(current_datetime()), timeout=100)
+            telegram_send_msg("Training iniziato...")
 
         model.fit_generator(train_sequence, epochs=num_epochs, callbacks=[keras.callbacks.LambdaCallback(
                             on_epoch_begin=lambda batch, logs: train_sequence.on_epoch_begin())]+training_callbacks)
 
         if chat_id != "undefined":
-            bot = telegram.Bot(token='591311395:AAEfSH464BdXSDezWGMZwdiLxLg2_aLlGDE')
-            bot.send_message(chat_id=chat_id, text="{} - Training completato!".format(current_datetime()), timeout=100)
+            telegram_send_msg("Training completato!")
 
         # ONLY WHEN U WANT USE THE TEST SET!!!
         # WARNING ONLY WHEN WE WANT THE TEST ERROR CAN BE DONE ONLY ONE TIME!
@@ -38,17 +36,17 @@ class SingletonTrain(object):
         # ====== save model ======
         # the three following instructions must be decommented when we want to save the model at the end of the training
         if chat_id != "undefined" and save_model:
-            bot = telegram.Bot(token='591311395:AAEfSH464BdXSDezWGMZwdiLxLg2_aLlGDE')
-            bot.send_message(chat_id=chat_id, text="{} - Sto salvando il modello".format(current_datetime()), timeout=100)
+            telegram_send_msg("Sto salvando il modello")
 
         if save_model:
             model.save('trained_model/{}.h5'.format(current_datetime()))
 
         if chat_id != "undefined" and save_model:
-            bot.send_message(chat_id=chat_id, text="{} - Modello salvato!".format(current_datetime()), timeout=100)
+            telegram_send_msg("{} - Modello salvato!")
 
     y_next_epoch = []
     x_next_epoch = []
+
     def Train(self, model, training_dataset_folder_name, epochs, batch_size, epochs_with_same_data,
               training_folders_count, validation_x, validation_y, to_avoid, validate_every, class_weight, enable_telegram_bot=False):
         t = None
@@ -94,8 +92,8 @@ class SingletonTrain(object):
 
     def load_data(self, folders, folders_to_load=15, to_avoid=[]):
         # print("\nstarted the fetch of data for the next epoch in parallel")
-        self.x_next_epoch, self.y_next_epoch, loaded_folders_list = LoadData.GetData(folders, limit_value=folders_to_load,
-                                                                           to_avoid=to_avoid)
+        self.x_next_epoch, self.y_next_epoch, loaded_folders_list = LoadData.GetData(folders,limit_value=folders_to_load,
+                                                                                     to_avoid=to_avoid)
         self.y_next_epoch = np_utils.to_categorical(self.y_next_epoch, 2)
         self.x_next_epoch = self.x_next_epoch.astype('float32')
         self.x_next_epoch /= np.max(self.x_next_epoch)
